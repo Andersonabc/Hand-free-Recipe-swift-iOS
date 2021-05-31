661cdf0fcd96208abbe458cfec0bb1ad6838e254
guyleaf <ychhua1@gmail.com>
2021-05-31 20:59:12 +0800

    Add basic project files

---
+++ Hand-free-Recipe/Hand_free_RecipeApp.swift
@@ -0,0 +1,20 @@
+//
+//  Hand_free_RecipeApp.swift
+//  Hand-free-Recipe
+//
+//  Created by Guyleaf on 2021/5/31.
+//
+
+import SwiftUI
+
+@main
+struct Hand_free_RecipeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}

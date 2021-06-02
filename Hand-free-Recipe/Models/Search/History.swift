8f01ef8cd9e401c7bce94fd5aa45718741cf356f
107590023 <t107590023@ntut.org.tw>
2021-06-02 17:33:59 +0800

    Add search view

---
+++ Hand-free-Recipe/Models/Search/History.swift
@@ -0,0 +1,32 @@
+//
+//  History.swift
+//  Hand-free-Recipe
+//
+//  Created by 李承紘 on 2021/6/2.
+//
+
+import Foundation
+
+
+struct History{
+    var data: [SearchRecord] = [SearchRecord(name: "Taco"),
+                                SearchRecord(name: "Chicken and waffles")]
+    var dataString: [String] = ["Taco","Chicken and waffles"]
+
+    
+    mutating func deleteHist(at record: SearchRecord){
+        if let index = data.firstIndex(where: {$0 === record}){
+            self.data.remove(at: index)
+            self.dataString.remove(at: index)
+        }
+    }
+    
+    mutating func appendHist(at record: SearchRecord){
+        print("going to append")
+        if !self.dataString.contains(record.name){
+            print("successfully append")
+            self.data.append(record)
+            self.dataString.append(record.name)
+        }
+    }
+}

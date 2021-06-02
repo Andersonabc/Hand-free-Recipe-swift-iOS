dfd48a74678bb7543ea74cf8e17f514bca460016
guyleaf <ychhua1@gmail.com>
2021-06-02 12:25:37 +0800

    Add Recipe Model

---
+++ Hand-free-Recipe/Models/Recipe/Recipe.swift
@@ -0,0 +1,54 @@
+//
+//  Recipe.swift
+//  Hand-free-Recipe
+//
+//  Created by Steven Shen on 2021/5/31.
+//
+
+import Foundation
+
+struct Recipe {
+    var coverImage: String {
+        get {
+            return self._coverImage
+        }
+    }
+    
+    var name: String {
+        get {
+            self._name
+        }
+    }
+    
+    var estimatedTime: Int {
+        get {
+            return self._estimatedTime
+        }
+    }
+    
+    var ingredients: [Ingredients] {
+        get {
+            return self._ingredients
+        }
+    }
+    
+    var steps: [RecipeStep] {
+        get {
+            return self._steps
+        }
+    }
+    
+    init(name: String, coverImage: String, estimatedTime: Int, ingredients: [Ingredients], steps: [RecipeStep]) {
+        self._name = name
+        self._coverImage = coverImage
+        self._estimatedTime = estimatedTime
+        self._ingredients = ingredients
+        self._steps = steps
+    }
+
+    private let _name: String
+    private let _coverImage: String
+    private let _estimatedTime: Int
+    private let _ingredients: [Ingredients]
+    private let _steps: [RecipeStep]
+}

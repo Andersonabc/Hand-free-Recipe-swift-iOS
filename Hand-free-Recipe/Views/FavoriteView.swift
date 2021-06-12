5e7b8bb2dcd596907a056899fc61e429d0a700bd
Steven Shen <chehsius@gmail.com>
2021-06-12 13:25:15 +0800

    SlideTabView

---
+++ Hand-free-Recipe/Views/FavoriteView.swift
@@ -0,0 +1,20 @@
+//
+//  FavoriteView.swift
+//  Hand-free-Recipe
+//
+//  Created by Steven Shen on 2021/6/2.
+//
+
+import SwiftUI
+
+struct FavoriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoriteView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteView()
+    }
+}
---
+++ Hand-free-Recipe/Views/SlideTabView.swift
@@ -0,0 +1,20 @@
+//
+//  SlideTabView.swift
+//  Hand-free-Recipe
+//
+//  Created by Steven Shen on 2021/6/5.
+//
+
+import SwiftUI
+
+struct SlideTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SlideTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        SlideTabView()
+    }
+}
